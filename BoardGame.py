584bb30b51e2c0fe3cdfd9e31aa1691e9afe67f1
Gareth M. <gkm25@protonmail.com>
2025-11-06 14:39:30 -0600

    event file

--- BoardGame.py
+++ BoardGame.py
@@ -2,6 +2,7 @@
 #Logic is in this python file
 
 from flask import Flask, render_template, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +10,24 @@ app = Flask(__name__)
 current_player = 1
 player1_pos = 0
 player2_pos = 0
+
+def saveTurn(turndata):
+    with open("eventfile.csv", "a+") as eventfile:
+        turn = {
+            "positions": {
+                "Player1": turndata['positions']['1'],
+                "Player2": turndata['positions']['2']
+            },
+            "scores": {
+                "Player1": turndata['scores']['1'],
+                "Player2": turndata['scores']['2']
+            },
+            "turn": turndata['turn']
+        }
+        eventfile.write(str(turndata['positions']['1'])+","+str(turndata['positions']['2'])+","+str(turndata['scores']['1'])+","+str(turndata['scores']['2'])+","+str(turndata['turn'])+"\n")
+        
+        
+            
 
 @app.route('/')
 def index():
@@ -24,8 +43,11 @@ def add_tile():
 @app.route('/update-events', methods=["POST"])
 def player_moved():
     if request.method == "POST":
-        print(request.get_data())
+        #print(request.get_json())
+        saveTurn(request.get_json())
     return "success"
 
 if __name__ == "__main__":
+    with open("eventfile.csv", "w+") as eventfile:
+        eventfile.write("Player1Position,Player2Position,Player1Score,Player2Score,Turn\n")
     app.run(debug=True)
